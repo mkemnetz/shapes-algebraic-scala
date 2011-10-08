9111e0a35c52dfb988f9f8cf3ef1013747abf198
Matt Kemnetz <kemnetz.m@gmail.com>
2011-10-08 13:44:04 -0500

    Edited moreShapes and started on ExtendedShapeAlgebra

--- src/edu/luc/cs/laufer/cs473/shapealgebra/Draw.scala
+++ src/edu/luc/cs/laufer/cs473/shapealgebra/Draw.scala
@@ -6,11 +6,13 @@ class Draw {
   def draw(g: Graphics2D)(s: Shape): Unit = s match {
     case Ellipse(hw, hh) => g.drawArc(-hw, -hh, 2 * hw, 2 * hh, 0, 360)
     case Rectangle(w, h) => g.drawRect(0, 0, w, h)
-    case Location(x: Int, y: Int, shape: Shape) => 
+    case Location(x: Int, y: Int, shape: Shape) => {
       g.translate(x, y)
       draw(g)(shape)
-    case Group(shapes @ _*) =>
+    }
+    case Group(shapes @ _*) => {
       shapes.map(z => draw(g)(z))
+    }
   }
 }
 
--- src/edu/luc/cs/laufer/cs473/shapealgebra/ExtendedShapeAlgebra.scala
+++ src/edu/luc/cs/laufer/cs473/shapealgebra/ExtendedShapeAlgebra.scala
@@ -1,19 +1,28 @@
-//package edu.luc.cs.laufer.cs473.shapealgebra
-//
-///**
-// * The category of shape algebras extended to support additional shapes.
-// */
-//trait ExtendedShapeAlgebra[R] extends ShapeAlgebra[R] {
-//
-//  def visitPolygon(p: Polygon): R
-//  // TODO: add missing visit methods similarly to Location
-//
-//  /**
-//   * The extended catamorphism for shapes.
-//   */
-//  override def fold(s: Shape): R = s match {
-//    case p: Polygon => visitPolygon(p)
-//    // TODO: add missing cases similarly to Location
-//    case _ => super.fold(s)
-//  }
-//}
+package edu.luc.cs.laufer.cs473.shapealgebra
+
+/**
+ * The category of shape algebras extended to support additional shapes.
+ */
+import java.awt.Color
+import java.awt.Point
+
+trait ExtendedShapeAlgebra[R] extends ShapeAlgebra[R] {
+
+  def visitStroke(r:R, s:Stroke):R
+  def visitFill(f:Shape):R
+  def vivitOutline(s:Shape):R
+  def visitPolygon(p: Polygon): R
+  def visitRotate(int:Int,s:Shape):R
+  def visitCircle(c:Circle): R
+  
+  // TODO: add missing visit methods similarly to Location
+
+  /**
+   * The extended catamorphism for shapes.
+   */
+  override def fold(s: Shape): R = s match {
+    case p: Polygon => visitPolygon(p)
+    // TODO: add missing cases similarly to Location
+    case _ => super.fold(s)
+  }
+}
